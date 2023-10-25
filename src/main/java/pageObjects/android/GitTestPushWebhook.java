1886f85627b7b3b67f713af122fba1a6760666ad
HeraldDC <herald.delacruz@collaberadigital.com>
2023-10-25 12:42:44 +0800

    webhooks2

--- src/main/java/pageObjects/android/GitTestPushWebhook.java
+++ src/main/java/pageObjects/android/GitTestPushWebhook.java
@@ -5,7 +5,7 @@ public class GitTestPushWebhook {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git test Web hooks");
-		System.out.println("Git test Web hooks5");
+		System.out.println("Git test Web hooks6");
 	}
 
 }
