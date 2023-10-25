1d10906e12daa020f98cf37a3c11b4c431bc1300
HeraldDC <herald.delacruz@collaberadigital.com>
2023-10-25 12:22:26 +0800

    git test webhooks

---
+++ src/main/java/pageObjects/android/GitTestPushWebhook.java
@@ -0,0 +1,10 @@
+package pageObjects.android;
+
+public class GitTestPushWebhook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git test Web hooks");
+	}
+
+}
